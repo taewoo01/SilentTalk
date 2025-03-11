8010ae0a33e1fe0f16e939edbf756b4cd96493cf
taewoo01 <xodn9402@naver.com>
2025-03-11 10:38:23 +0900

    파일 구조 완료

--- handtalk-app/src/index.js
+++ handtalk-app/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';  // 서비스 워커 파일 추가
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,8 +10,3 @@ root.render(
   </React.StrictMode>
 );
 
-// 서비스 워커 등록
-serviceWorkerRegistration.register();  // PWA 기능 활성화
-
-// 웹 성능 측정을 원하면 아래 코드 사용
-reportWebVitals();
